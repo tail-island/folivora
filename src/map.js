7891221c61f3f3492fa83697ecf47e3a65d2759c
OJIMA Ryoji <rojima1@gmail.com>
2018-04-19 11:30:58 +0900

    node version is upgraded.

--- src/map.js
+++ src/map.js
@@ -1,14 +1,21 @@
 import some from './some';
 
 export default function* map(f, ...colls) {
-  const iters = colls.map(coll => coll[Symbol.iterator]());
-
-  for (;;) {
-    const nexts = iters.map(iter => iter.next());
-    if (nexts.some(next => next.done)) {
-      break;
+  if (colls.length === 1) {
+    for (const x of colls[0]) {
+      yield f(x);
     }
 
-    yield (f(...nexts.map(next => next.value)));
+  } else {
+    const iters = colls.map(coll => coll[Symbol.iterator]());
+
+    for (;;) {
+      const nexts = iters.map(iter => iter.next());
+      if (nexts.some(next => next.done)) {
+        break;
+      }
+
+      yield (f(...nexts.map(next => next.value)));
+    }
   }
 }
